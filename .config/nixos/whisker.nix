f46e41c029663d3efa95816e6fb44835639d3e56
Kevin J Hoerr <kjhoerr@protonmail.com>
2022-12-28 10:17:14 -0500

    Enable grub to enable menu entry for W11

--- .config/nixos/whisker.nix
+++ .config/nixos/whisker.nix
@@ -11,9 +11,34 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  time.hardwareClockInLocalTime = true;
+  boot.supportedFilesystems = [ "ntfs" ];
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub = {
+    # despite what the configuration.nix manpage seems to indicate,
+    # as of release 17.09, setting device to "nodev" will still call
+    # `grub-install` if efiSupport is true
+    # (the devices list is not used by the EFI grub install,
+    # but must be set to some value in order to pass an assert in grub.nix)
+    device = "nodev";
+    efiSupport = true;
+    enableCryptodisk = true;
+    enable = true;
+    # set $FS_UUID to the UUID of the EFI partition
+    #extraEntries = ''
+    #  menuentry "Windows 11" {
+    #    insmod part_gpt
+    #    insmod fat
+    #    insmod search_fs_uuid
+    #    insmod chain
+    #    search --fs-uuid --set=root 569D-4A46
+    #    chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+    #  }
+    #'';
+    version = 2;
+    useOSProber = true;
+  };
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Setup keyfile
