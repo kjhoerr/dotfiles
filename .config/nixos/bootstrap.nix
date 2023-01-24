573d40351d4866d1b419d989207aa4d40667e1e2
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-01-24 13:36:12 -0500

    Add more comment

--- .config/nixos/bootstrap.nix
+++ .config/nixos/bootstrap.nix
@@ -1,6 +1,7 @@
 # bootstrap.nix
 # Designed as first-boot configuration (bootstrap) for impermanence systems
 # Run nixos-generate-config --root=/mnt for hardware scan then overwrite with this as configuration.nix
+# Then you can reboot and get SecureBoot, TPM, /persist, and whatever else set up and use flakes to install
 { config, pkgs, lib, ... }: {
   imports =
     [ # Include the results of the hardware scan.
