48b0bfe9adf3cfe100a2463fbd27ec675ccbb789
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-01-26 19:31:20 -0500

    flake.nix: Add comments, use functions

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Secure Boot for NixOS
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # User profile manager based on Nix
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Links persistent folders into system
     impermanence.url = "github:nix-community/impermanence";
+
+    # Provides module support for specific vendor hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
@@ -20,44 +28,55 @@
         inherit system;
         config.allowUnfree = true;
       };
+
+      # Base user config modules to be overwritten
       homeModules = [ ./.config/nixos/home ];
+
+      # Base OS configs, adapts to system configs
       osModules = [
         inputs.lanzaboote.nixosModules.lanzaboote
         inputs.impermanence.nixosModules.impermanence
         ./.config/nixos/os
       ];
+
+      # Function to build a home configuration from user modules
+      homeUser = (userModules: home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # userModules overwrites, so is appended
+        modules = homeModules ++ userModules;
+      });
+
+      # Function to build a nixos configuration from system modules
+      nixosSystem = (systemModules: nixpkgs.lib.nixosSystem {
+        inherit system;
+        # osModules depends on some values from systemModules, so is appended
+        modules = systemModules ++ osModules;
+      });
+
     in {
       homeConfigurations = {
-        kjhoerr = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = homeModules ++ [ ./.config/nixos/users/kjhoerr.nix ];
-        };
-        khoerr = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = homeModules ++ [ ./.config/nixos/users/khoerr.nix ];
-        };
+
+        khoerr = homeUser [ ./.config/nixos/users/khoerr.nix ];
+
+        kjhoerr = homeUser [ ./.config/nixos/users/kjhoerr.nix ];
+
       };
       nixosConfigurations = {
-        ariadne = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            inputs.nixos-hardware.nixosModules.framework
-            ./.config/nixos/systems/ariadne.nix
-          ] ++ osModules;
-        };
-        cronos = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p1-gen3
-            ./.config/nixos/systems/cronos.nix
-          ] ++ osModules;
-        };
-        whisker = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./.config/nixos/systems/whisker.nix
-          ] ++ osModules;
-        };
+
+        ariadne = nixosSystem [
+          inputs.nixos-hardware.nixosModules.framework
+          ./.config/nixos/systems/ariadne.nix
+        ];
+
+        cronos = nixosSystem [
+          inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p1-gen3
+          ./.config/nixos/systems/cronos.nix
+        ];
+
+        whisker = nixosSystem [
+          ./.config/nixos/systems/whisker.nix
+        ];
+
       };
     };
 }
