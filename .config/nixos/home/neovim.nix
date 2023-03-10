e73214cc68f9981dfea12e5ffb170d196bf42af0
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-03-09 22:37:45 -0500

    neovim.nix: remove unused neovim plugins

--- .config/nixos/home/neovim.nix
+++ .config/nixos/home/neovim.nix
@@ -3,7 +3,6 @@
 
   programs.neovim.enable = lib.mkDefault true;
   programs.neovim.vimAlias = lib.mkDefault true;
-  programs.neovim.defaultEditor = lib.mkDefault true;
   programs.neovim.extraConfig = lib.mkDefault ''
     set nocompatible
     set showmatch
@@ -21,38 +20,6 @@
       colorscheme dracula
     endif
   '';
-  programs.neovim.plugins = lib.mkDefault (with pkgs.vimPlugins; [
-    bufferline-nvim
-    dracula-vim
-    nvim-colorizer-lua
-    nvim-tree-lua
-    tokyonight-nvim
-    telescope-fzf-native-nvim
-    telescope-nvim
-    gitsigns-nvim
-    (nvim-treesitter.withPlugins (plugins: with plugins; [
-      tree-sitter-bash
-      tree-sitter-dockerfile
-      tree-sitter-html
-      tree-sitter-java
-      tree-sitter-javascript
-      tree-sitter-json
-      tree-sitter-markdown
-      tree-sitter-nix
-      tree-sitter-regex
-    ]))
-  ]);
-
-  programs.neovim.extraPackages = lib.mkDefault (with pkgs; [
-    nodePackages.bash-language-server
-    nodePackages.dockerfile-language-server-nodejs
-    hadolint
-    nodePackages.vim-language-server
-    shellcheck
-    rnix-lsp
-    deadnix
-    statix
-  ]);
 
 }
 
