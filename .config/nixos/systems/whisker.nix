5b52e04aa3537f74882f103f6aa0bd7869dffa24
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-04-06 10:41:32 -0400

    whisker.nix: Add lutris to packages

--- .config/nixos/systems/whisker.nix
+++ .config/nixos/systems/whisker.nix
@@ -83,6 +83,8 @@
     dedicatedServer.openFirewall = true;
   };
 
+  environment.systemPackages = lib.mkAfter [ pkgs.lutris ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
