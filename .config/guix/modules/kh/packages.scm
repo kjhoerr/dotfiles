aebbd80f9b7134d11eae3ff74b4a74d3a2ed60bc
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-04-10 18:16:00 -0400

    -S
    packages.scm: Clean up package lists

--- .config/guix/modules/kh/packages.scm
+++ .config/guix/modules/kh/packages.scm
@@ -6,21 +6,16 @@
  #:use-module ((gnu packages admin) #:select (dmidecode inetutils neofetch pfetch))
  #:use-module ((gnu packages certs) #:select (nss-certs))
  #:use-module ((gnu packages compression) #:select (unzip zip))
- #:use-module ((gnu packages emacs) #:select (emacs))
- #:use-module ((gnu packages emacs-xyz) #:select (emacs-guix))
  #:use-module ((gnu packages file) #:select (file))
  #:use-module ((gnu packages fonts) #:select (font-awesome font-fira-code font-google-noto font-ibm-plex font-openmoji font-wqy-zenhei))
- #:use-module ((gnu packages freedesktop) #:select (fprintd libfprint))
- #:use-module ((gnu packages games) #:select (steam-devices-udev-rules))
- #:use-module ((gnu packages gnome) #:select (dconf gnome-tweaks gsettings-desktop-schemas))
+ #:use-module ((gnu packages gnome) #:select (dconf gnome-tweaks))
  #:use-module ((gnu packages gnupg) #:select (gnupg pinentry pinentry-gnome3))
- #:use-module ((gnu packages linux) #:select (powertop tlp))
+ #:use-module ((gnu packages linux) #:select (powertop))
  #:use-module ((gnu packages package-management) #:select (nix))
- #:use-module ((gnu packages security-token) #:select (libu2f-host libyubikey yubikey-personalization))
+ #:use-module ((gnu packages security-token) #:select (libu2f-host libfido2 yubikey-personalization))
  #:use-module ((gnu packages ssh) #:select (openssh))
  #:use-module ((gnu packages version-control) #:select (git))
- #:use-module ((gnu packages vim) #:select (neovim neovim-packer neovim-syntastic))
- #:use-module ((nongnu packages firmware) #:select (fwupd-nonfree)))
+ #:use-module ((gnu packages vim) #:select (neovim)))
 
 (define %font-packages 
   (list
@@ -34,7 +29,6 @@
 (define %gnome-packages
   (list
     dconf
-    gsettings-desktop-schemas
     gnome-tweaks
     pinentry-gnome3))
 
@@ -43,13 +37,10 @@
     dmidecode
     file
     fprintd
-    fwupd-nonfree
     git
     inetutils
     neofetch
     neovim
-    neovim-packer
-    neovim-syntastic
     nix
     openssh
     pfetch
@@ -60,6 +51,8 @@
 (define %req-packages
   (list
     gnupg
+    libfido2
+    libu2f-host
     nss-certs
     pinentry
     yubikey-personalization))
