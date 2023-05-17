a60b18d79e32fd19a8189cf7810ff6245ee3c2fa
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-05-17 19:57:46 -0400

    user.nix: Add desktop entry to force wayland for microsoft-edge-dev

--- .config/nixos/home/user.nix
+++ .config/nixos/home/user.nix
@@ -23,5 +23,16 @@
     microsoft-edge-dev
   ]);
 
+  xdg.desktopEntries.microsoft-edge-dev = {
+    name = "Microsoft Edge (dev)";
+    genericName = "Web Browser";
+    exec = "microsoft-edge-dev -enable-features=UseOzonePlatform -ozone-platform=wayland %U";
+    terminal = false;
+    icon = "microsoft-edge-dev";
+    type = "Application";
+    categories = [ "Network" "WebBrowser" ];
+    mimeType = [ "application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml" "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png" "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https"];
+  };
+
 }
 
