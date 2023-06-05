c80490f2ab0bbb14f49a12bc3e7c524b0d2547ed
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-06-04 21:12:20 -0400

    persist.nix: Add root-diff script for checking impermanence files

--- .config/nixos/os/persist.nix
+++ .config/nixos/os/persist.nix
@@ -1,6 +1,46 @@
 # persist.nix
 # Requires impermanence flake
-{ lib, ... }: {
+{ lib, pkgs, ... }:
+let
+  root-diff = pkgs.writeShellScriptBin "root-diff" ''
+    #!/usr/bin/env bash
+    # Check current root for any files that are not persisted.
+    # (These files will be lost on a reboot.)
+
+    set -euo pipefail
+
+    sudo mkdir -p /mnt/tmp-root
+
+    # We first mount the btrfs root to /mnt/tmp-root
+    # so we can check the subvolumes for mismatching files.
+    # If LVM exists, mount that.
+    if [[ -b /dev/pool/root ]]; then
+      sudo mount -t btrfs -o subvol=/ /dev/pool/root /mnt/tmp-root
+    else
+      sudo mount -t btrfs -o subvol=/ /dev/mapper/enc /mnt/tmp-root
+    fi
+
+    OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/tmp-root/root-blank 9999999 | awk '{print $NF}')
+
+    sudo btrfs subvolume find-new "/mnt/tmp-root/root" "$OLD_TRANSID" |
+      sed '$d' |
+      cut -f17- -d' ' |
+      sort |
+      uniq |
+      while read path; do
+        path="/$path"
+        if [ -L "$path" ]; then
+          : # The path is a symbolic link, so is probably handled by NixOS already
+        elif [ -d "$path" ]; then
+          : # The path is a directory; ignore
+        else
+          echo "$path"
+        fi
+      done
+
+    sudo umount /mnt/tmp-root
+  '';
+in {
 
   boot.initrd.systemd.enable = lib.mkDefault true;
   boot.initrd.systemd.services.rollback = {
@@ -101,5 +141,7 @@
       "/var/lib/power-profiles-daemon/state.ini"
     ];
   };
+
+  environment.systemPackages = lib.mkBefore [ root-diff ];
 
 }
