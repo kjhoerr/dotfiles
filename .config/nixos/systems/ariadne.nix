209e4c0868b0d89b7ed0201c5d10ee6c07cda0d6
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-06-12 08:21:22 -0400

    ariadne.nix: Add steam to programs

--- .config/nixos/systems/ariadne.nix
+++ .config/nixos/systems/ariadne.nix
@@ -111,6 +111,8 @@
     passwordFile = "/persist/passwords/kjhoerr";
   };
 
+  programs.steam.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
