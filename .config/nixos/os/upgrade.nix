b3b812f8a14a8a05e800f1d87cf18bd8c060fb7e
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-07-11 11:04:47 -0400

    upgrade.nix: Add nix-community binary cache

--- .config/nixos/os/upgrade.nix
+++ .config/nixos/os/upgrade.nix
@@ -10,6 +10,10 @@
   nix.gc.automatic = lib.mkDefault true;
   nix.gc.options = lib.mkDefault "--delete-older-than 14d";
 
+  # Enable nix-community public binary cache, for potential build skips on flakes
+  nix.settings.substituters = lib.mkDefault [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
+  nix.settings.trusted-public-keys = lib.mkDefault [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+
   # Disable network targets due to common upgrade issues
   systemd = {
     targets.network-online.wantedBy = lib.mkForce []; # Normally ["multi-user.target"]
