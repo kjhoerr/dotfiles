81108d518ae5f9ab98d62342635c0639063d23b4
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-09-19 11:43:38 -0400

    flake.nix: Switch home-manager to master branch to support unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
     # User profile manager based on Nix
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
