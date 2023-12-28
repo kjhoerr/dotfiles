bb8caa4d37bb01f88dc30918d7203dd19e091598
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-12-28 10:52:49 -0500

    flake.nix: Add PPD patches for pstate support

--- flake.nix
+++ flake.nix
@@ -50,6 +50,37 @@
       };
       osOverlays = [
         (_: _: { fw-ectool = inputs.fw-ectool.packages.${system}.ectool; })
+        (final: prev: {
+            power-profiles-daemon = prev.power-profiles-daemon.overrideAttrs(
+              old: {
+                version = "0.13-1";
+
+                patches = (old.patches or []) ++ [
+                   (prev.fetchpatch {
+                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/127.patch"; 
+                      sha256= "sha256-jnq5yJvWQHOlZ78SE/4/HqiQfF25YHQH/T4wwDVRHR0=";
+                   })
+                   (prev.fetchpatch {
+                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/128.patch"; 
+                      sha256 = "sha256-YD9wn9IQlCp02r4lmwRnx9Eur2VVP1JfC/Bm8hlzF3Q=";
+                   })
+                   (prev.fetchpatch {
+                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/129.patch"; 
+                      sha256 = "sha256-9T+I3BAUW3u4LldF85ctE0/PLu9u+KBN4maoL653WJU=";
+                   })
+                ];
+
+                # explicitly fetching the source to make sure we're patching over 0.13 (this isn't strictly needed):
+                src = prev.fetchFromGitLab {
+                  domain = "gitlab.freedesktop.org";
+                  owner = "hadess";
+                  repo = "power-profiles-daemon";
+                  rev = "0.13";
+                  sha256 = "sha256-ErHy+shxZQ/aCryGhovmJ6KmAMt9OZeQGDbHIkC0vUE=";
+                };
+              });
+            }
+        )
       ];
 
       # Base user config modules
