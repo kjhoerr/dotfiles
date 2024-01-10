4935e2b9c57d157b0043eafcb63e36fe6921ee33
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-01-09 20:11:01 -0500

    flake.nix: Switch to superm1 fork until ppd 14 is ready

--- flake.nix
+++ flake.nix
@@ -53,30 +53,15 @@
         (final: prev: {
             power-profiles-daemon = prev.power-profiles-daemon.overrideAttrs(
               old: {
-                version = "0.13-1";
-
-                patches = (old.patches or []) ++ [
-                   (prev.fetchpatch {
-                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/127.patch"; 
-                      sha256= "sha256-jnq5yJvWQHOlZ78SE/4/HqiQfF25YHQH/T4wwDVRHR0=";
-                   })
-                   (prev.fetchpatch {
-                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/128.patch"; 
-                      sha256 = "sha256-YD9wn9IQlCp02r4lmwRnx9Eur2VVP1JfC/Bm8hlzF3Q=";
-                   })
-                   (prev.fetchpatch {
-                      url = "https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/merge_requests/129.patch"; 
-                      sha256 = "sha256-9T+I3BAUW3u4LldF85ctE0/PLu9u+KBN4maoL653WJU=";
-                   })
-                ];
+                version = "0.13-2";
 
                 # explicitly fetching the source to make sure we're patching over 0.13 (this isn't strictly needed):
                 src = prev.fetchFromGitLab {
                   domain = "gitlab.freedesktop.org";
-                  owner = "hadess";
+                  owner = "superm1";
                   repo = "power-profiles-daemon";
-                  rev = "0.13";
-                  sha256 = "sha256-ErHy+shxZQ/aCryGhovmJ6KmAMt9OZeQGDbHIkC0vUE=";
+                  rev = "mlimonci/pre-commit";
+                  sha256 = "sha256-4+V9UaCFwVRgla73YAmPQHg33lPd+dCaVZRNs5g4z8g=";
                 };
               });
             }
