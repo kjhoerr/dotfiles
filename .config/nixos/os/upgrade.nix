19299f0dc3bcd29adb3b8cd5ebbc38310536e9c2
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-02-21 01:34:46 -0500

    upgrade.nix: Expand ssh, trusted-users settings
    Want to investigate using remote builders and remote stores to cache
    updates, since my systems are often updated at similar times using the
    same target nixpkgs.
    
    Ideally I will be able to expand this to a VM/server remote that can act
    as the cache where my systems will be able to pull locally.

--- .config/nixos/os/upgrade.nix
+++ .config/nixos/os/upgrade.nix
@@ -13,6 +13,18 @@
   # Enable nix-community public binary cache, for potential build skips on flakes
   nix.settings.substituters = lib.mkDefault [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
   nix.settings.trusted-public-keys = lib.mkDefault [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  nix.settings.trusted-users = lib.mkDefault [ "root" "@wheel" ];
+
+  # Leave SSHD off by default, but set up sensible defaults for when it's enabled
+  services.openssh.enable = lib.mkDefault false;
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+  };
+
+  # Similar to SSHD config, leave off by default but add personal pubkey
+  nix.sshServe.enable = lib.mkDefault false;
+  nix.sshServe.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVH5c050+fT7lIYhycEVvbDx6+aNeDliEFTNLP2EULk openpgp:0x69ED7111" ];
 
   # Disable network targets due to common upgrade issues
   systemd = {
