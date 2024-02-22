fc17f40531e37a40423960aa29021294b77d7c1d
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-02-22 11:41:38 -0500

    persist.nix: Use root-reset.sh script for rollback service

--- .config/nixos/os/persist.nix
+++ .config/nixos/os/persist.nix
@@ -2,6 +2,7 @@
 # Requires impermanence flake
 { lib, pkgs, ... }:
 let
+  root-reset-src = builtins.readFile ../scripts/root-reset.sh;
   root-diff = pkgs.writeShellApplication {
     name = "root-diff";
     runtimeInputs = [ pkgs.btrfs-progs ];
@@ -24,53 +25,7 @@ in {
     ];
     unitConfig.DefaultDependencies = "no";
     serviceConfig.Type = "oneshot";
-    script = ''
-      mkdir -p /mnt
-
-      # Pick up any LVM from newly mapped enc
-      vgscan
-      vgchange -ay
-
-      # We first mount the btrfs root to /mnt
-      # so we can manipulate btrfs subvolumes.
-      # If LVM exists, mount that.
-      if [[ -b /dev/pool/root ]]; then
-        mount -t btrfs -o subvol=/ /dev/pool/root /mnt
-      else
-        mount -t btrfs -o subvol=/ /dev/mapper/enc /mnt
-      fi
-
-      # While we're tempted to just delete /root and create
-      # a new snapshot from /root-blank, /root is already
-      # populated at this point with a number of subvolumes,
-      # which makes `btrfs subvolume delete` fail.
-      # So, we remove them first.
-      #
-      # /root contains subvolumes:
-      # - /root/var/lib/portables
-      # - /root/var/lib/machines
-      #
-      # I suspect these are related to systemd-nspawn, but
-      # since I don't use it I'm not 100% sure.
-      # Anyhow, deleting these subvolumes hasn't resulted
-      # in any issues so far, except for fairly
-      # benign-looking errors from systemd-tmpfiles.
-      btrfs subvolume list -o /mnt/root |
-        cut -f9 -d' ' |
-        while read subvolume; do
-          echo "deleting /$subvolume subvolume..."
-          btrfs subvolume delete "/mnt/$subvolume"
-        done &&
-        echo "deleting /root subvolume..." &&
-        btrfs subvolume delete /mnt/root
-
-      echo "restoring blank /root subvolume..."
-      btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-      # Once we're done rolling back to a blank snapshot,
-      # we can unmount /mnt and continue on the boot process.
-      umount /mnt
-    '';
+    script = root-reset-src;
   };
   boot.initrd.systemd.services.persisted-files = {
     description = "Hard-link persisted files from /persist";
