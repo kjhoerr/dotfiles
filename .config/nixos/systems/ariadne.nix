911b86bc015e7c08370f70639714dbdd7daa7ff3
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-05-21 21:57:32 -0400

    ariadne.nix: Remove kernel patch spec

--- .config/nixos/systems/ariadne.nix
+++ .config/nixos/systems/ariadne.nix
@@ -15,16 +15,6 @@
     };
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
-
-    kernelPatches = [
-      {
-        name = "pinctrl";
-        patch = pkgs.fetchurl {
-          url = "https://lore.kernel.org/linux-gpio/20240519124109.1523-1-mario.limonciello@amd.com/t.mbox.gz";
-          hash = "sha256-eYREEFkWEzcCpZL+NmrlPrmt6ogY00GD+4KwSox/8vk=";
-        };
-      }
-    ];
   };
 
   fileSystems."/" =
