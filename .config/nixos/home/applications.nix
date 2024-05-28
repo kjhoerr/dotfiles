0a5e08f0d0cf34ac4471677ad8c605661b49ff87
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-05-28 11:16:14 -0400

    applications.nix: Switch Edge Dev for ungoogled-chromium

--- .config/nixos/home/applications.nix
+++ .config/nixos/home/applications.nix
@@ -13,22 +13,11 @@
     foliate
     gnumeric
     keepassxc
-    microsoft-edge-dev
     obsidian
     openlens
     switcheroo
+    ungoogled-chromium
   ]);
-
-  xdg.desktopEntries.microsoft-edge-dev = {
-    name = "Microsoft Edge (dev)";
-    genericName = "Web Browser";
-    exec = "${pkgs.microsoft-edge-dev}/bin/microsoft-edge-dev --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WebRTCPipeWireCapturer %U";
-    terminal = false;
-    icon = "microsoft-edge-dev";
-    type = "Application";
-    categories = [ "Network" "WebBrowser" ];
-    mimeType = [ "application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml" "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png" "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https"];
-  };
 
 }
 
