97c285e5c6225408e51f22d4d4a54fb5cc97a6ce
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-06-10 21:23:38 -0400

    system.nix: Enable libvirt

--- .config/nixos/os/system.nix
+++ .config/nixos/os/system.nix
@@ -121,6 +121,23 @@ in {
     dockerCompat = true;
   };
 
+  # libvert
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
   # Wayland-specific configuration
   services.xserver.displayManager.gdm.wayland = true;
   environment.sessionVariables = {
