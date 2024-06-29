3717e7629fc1e08b735cb4632188a0a8aeae226b
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-06-29 13:27:27 -0400

    secure-boot.nix: configure keepEnv for wheel group

--- .config/nixos/os/secure-boot.nix
+++ .config/nixos/os/secure-boot.nix
@@ -93,6 +93,10 @@ in {
   # madman: switch sudo with doas
   security.doas.enable = lib.mkDefault true;
   security.sudo.enable = lib.mkDefault false;
+  security.doas.extraRules = [{
+    groups = [ "wheel" ];
+    keepEnv = true;
+  }];
 
   # Bootspec and Secure Boot using lanzaboote
   #
@@ -108,12 +112,12 @@ in {
   # TPM for unlocking LUKS
   #
   # TPM kernel module must be enabled for initrd. Device driver is viewable via the command:
-  # sudo systemd-cryptenroll --tpm2-device=list
+  # systemd-cryptenroll --tpm2-device=list
   # And added to a device's configuration:
   # boot.initrd.kernelModules = [ "tpm_tis" ];
   #
   # Must be enabled by hand - e.g.
-  # sudo systemd-cryptenroll --wipe-slot=tpm2 /dev/nvme0n1p3 --tpm2-device=auto --tpm2-pcrs=0+2+7
+  # systemd-cryptenroll --wipe-slot=tpm2 /dev/nvme0n1p3 --tpm2-device=auto --tpm2-pcrs=0+2+7
   #
   security.tpm2.enable = true;
   security.tpm2.tctiEnvironment.enable = true;
