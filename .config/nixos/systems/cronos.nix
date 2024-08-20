dd06d0b528689b4df3e38f49b320cd17b929157b
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-08-20 11:45:29 -0400

    cronos.nix: Remove kernel spec locked to 6.9, to use latest

--- .config/nixos/systems/cronos.nix
+++ .config/nixos/systems/cronos.nix
@@ -6,7 +6,6 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "tpm_tis" ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_9;
   boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
   boot.blacklistedKernelModules = [
     "nouveau"
