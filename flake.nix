2ebb01403d581b20a152903fccdcac053d69f0f1
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-09-20 15:07:23 -0400

    flake.nix: Add lilyinstarlight/nixos-cosmic for Cosmic DE

--- flake.nix
+++ flake.nix
@@ -38,6 +38,12 @@
       url = "github:tlvince/ectool.nix";
       inputs.nixpkgs.follows = "nixos-pkgs";
     };
+
+    # Cosmic DE for NixOS
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixos-pkgs";
+    };
   };
 
   outputs = { nixpkgs, ... }@inputs:
@@ -78,6 +84,7 @@
         inputs.lanzaboote.nixosModules.lanzaboote
         inputs.impermanence.nixosModules.impermanence
         inputs.nixos-hardware.nixosModules.common-hidpi
+        inputs.nixos-cosmic.nixosModules.default
         ./.config/nixos/os/persist.nix
         ./.config/nixos/os/secure-boot.nix
         ./.config/nixos/os/system.nix
