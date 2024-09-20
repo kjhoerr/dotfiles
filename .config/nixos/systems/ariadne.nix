cadee4a2e048939639d0923a07b4c332ce788e8d
Kevin J Hoerr <kjhoerr@protonmail.com>
2024-09-20 13:57:54 -0400

    ariadne.nix: Adjust gnome package names that are now top-level

--- .config/nixos/systems/ariadne.nix
+++ .config/nixos/systems/ariadne.nix
@@ -73,7 +73,7 @@
       [org.gnome.mutter]
       experimental-features=['scale-monitor-framebuffer']
     '';
-    extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
+    extraGSettingsOverridePackages = [ pkgs.mutter ];
   };
 
   security.pam.services.login.fprintAuth = false;
@@ -85,12 +85,12 @@
     auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
     auth       optional                    pam_permit.so
     auth       required                    pam_env.so
-    auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
-    auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+    auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
+    auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
     account    include                     login
     password   required                    pam_deny.so
     session    include                     login
-    session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
   '';
 
   # Set display settings with 150% fractional scaling
