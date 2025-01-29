2980af7a0a1c608eaad6d7ba5fdd9483c4276dae
Kevin J Hoerr <kjhoerr@protonmail.com>
2025-01-29 17:04:07 -0500

    upgrade.nix: Ensure trusted-users is set

--- .config/nixos/os/upgrade.nix
+++ .config/nixos/os/upgrade.nix
@@ -23,7 +23,7 @@ in {
   # Enable nix-community public binary cache, for potential build skips on flakes
   nix.settings.substituters = lib.mkDefault [ "http://nix-cache.local:9080/" "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
   nix.settings.trusted-public-keys = lib.mkDefault [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-  nix.settings.trusted-users = lib.mkDefault [ "root" "@wheel" ];
+  nix.settings.trusted-users = [ "root" "@wheel" ];
 
   # Leave SSHD off by default, but set up sensible defaults for when it's enabled
   services.openssh.enable = lib.mkDefault false;
