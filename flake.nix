5a32f3368c302c8e3ef18574f63b42246bba4b63
Kevin J Hoerr <kjhoerr@protonmail.com>
2025-07-03 08:26:30 -0400

    flake.nix: Use tailscale version from nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -73,6 +73,9 @@
         ./.config/nixos/os/upgrade.nix
         {
           nixpkgs.overlays = osOverlays;
+
+          # Use most up-to-date tailscale package from nixpkgs-unstable
+          services.tailscale.package = pkgs.tailscale;
         }
       ];
 
